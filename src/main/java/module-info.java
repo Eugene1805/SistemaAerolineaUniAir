ea00f05c6e3b6bae0e854bd695d888e3b92ebb08
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-05-06 07:35:01 -0600

    Add FXML and Controllers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.sistemaaerolineauniair {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.sistemaaerolineauniair to javafx.fxml;
     exports com.mycompany.sistemaaerolineauniair;
